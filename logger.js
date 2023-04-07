5d8cbece478ddb8005627083c2415dadc9031622
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-04-06 21:30:00 -0500

    new files

--- logger.js
+++ logger.js
@@ -1,12 +1,26 @@
 // (function(exports, require, module, __filename, __dirname) {
 
+
+const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+
     console.log(__filename);
     console.log(__dirname);
 
 var url ='http://mylogger.io/log';
-function log(message) {
-    console.log(message);
+
+class Logger extends EventEmitter  {
+    
+    log(message) {
+        console.log(message);
+    
+        // raise an event
+    this.emit('messageLogged', { id: 1, url: 'http://localhost'});
+    
+    //raise: logging (data: message)
 }
-module.exports = log;
+
+}
+module.exports = Logger;
 // })
 // module.exports.endPont = url;
--- scripts.js
+++ scripts.js
@@ -38,3 +38,58 @@ var freeMemory = os.freemem();
 console.log('Total Memory: ' + totalMemory);
 
 console.log(`Free Memory:  ${freeMemory}`);
+
+const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+
+// console.log(files);
+
+fs.readdir('./', function(err, files) {
+if (err) console.log('Error', err);
+else  console.log('Result', files);
+
+});
+const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+
+// emitter.addListener('error', function(err) {
+
+// emitter.on('messageLogged', function(arg) {
+// console.log('listener called', arg);
+// });
+// register a listener
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
+    console.log('listener called', arg);
+    });
+
+    logger.log('message');
+    logger.log('messageLogged');
+
+    const http = require('http');
+
+    const server = http.createServer((req, res) => {
+        
+        if (req.url === '/') {
+            res.write('hello world');
+            res.end();
+        }
+
+        if (req.url === 'api/courses') {
+            res.write(JSON.stringify([1, 2, 3]));
+            res.end();
+        }
+
+    });
+
+    // server.on('error', (err) => {throw err});
+    // server.on('connection', (socket) => {
+    //     console.log('New connection...');
+    // });
+
+    server.listen(3001);
+    console.log('listening on port 3001...');
